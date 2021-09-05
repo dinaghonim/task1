e604a615d655fff9e7a09a7cb27ef620f2f3296f
dinaghonim <90148752+dinaghonim@users.noreply.github.com>
2021-09-06 01:41:18 +0200

    Update task.php

--- task.php
+++ task.php
@@ -1,24 +1,25 @@
 <?php
 
-$bill = 1000;
+$bill = 51;
 
 
 switch($bill)
 {
-	case $bill<=50:
+	case ($bill<=50):
 		echo("value"." = ". $bill * 3.50 ." "."$");
 		break;
 		
-	case $bill<=150:
+	case (($bill>50)&&($bill<=150)):
 		echo("value"." = ". $bill * 4 ." "."$");
 		break;
 		
-	case $bill>150:
+	case ($bill>150):
 		echo("value"." = ". $bill * 6.5 ." "."$");
 		break;
+	default:
+		echo('no');
 		
 }
-
 
 
 ?>
